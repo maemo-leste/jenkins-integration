dc94b38985589adca0e792d8f9f6a5ddcfbd6175
Merlijn Wajer <merlijn@wizzup.org>
2017-09-13 11:25:27 +0200

    jobs: Add everything for hildon-desktop (minus imported packages)

--- config.py
+++ config.py
@@ -24,14 +24,84 @@ _jobs =  {
     # 
     'iphbd': {},
 
-    # 
-    'libmatchbox2': {},
-
     # Maemo has its own gtk2 fork
     'gtk': {}, # maybe call the job maemo-gtk2?
 
     # 
+    'libmatchbox2': {},
+
+    #
     'mce-dev': {},
+
+    #
+    'libhildonmime': {},
+
+    #
+    'cityinfo': {},
+
+    #
+    'clockd': {},
+
+    #
+    'libosso': {},
+
+    #
+    'icd2-osso-ic-dev': {},
+
+    #
+    'libconic': {},
+
+    #
+    'osso-systemui-dbus-dev': {},
+
+    #
+    'libdsme': {},
+
+    #
+    'statusbar-alarm-dbus-api': {},
+
+    #
+    'hildon': {},
+
+    #
+    'maemo-system-services': {},
+
+    #
+    'osso-core-config': {},
+
+    #
+    'osso-af-utils': {},
+
+    #
+    'osso-af-startup': {},
+
+    #
+    'osso-af-settings': {},
+
+    #
+    'ke-recv-extra': {},
+
+    #
+    'upstart-dev': {},
+
+    # fixed with a hack, remember to fix properly
+    # https://git.devuan.org/maemo/clipboard-manager/commit/df4d727d4dc95cd01014dc388c9c1088c8a296f5
+    'clipboard-manager': {},
+
+    #
+    'libhildondesktop': {},
+
+    #
+    'alarmd': {},
+
+    #
+    'maemo-launcher': {},
+
+    #
+    'profiled': {},
+
+    #
+    'hildon-desktop': {},
 }
 
 def get_jobs():
