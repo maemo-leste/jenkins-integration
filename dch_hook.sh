4dfa2bfa412bb83334e085ffe85ac4e17e5edce7
Arthur Demchenkov <spinal.by@gmail.com>
2020-01-24 03:28:03 +0300

    dch_hook.sh: minor fixes

--- dch_hook.sh
+++ dch_hook.sh
@@ -22,19 +22,19 @@ _srcinfo=$(dpkg-parsechangelog -n 1 -l debian/changelog)
 _srcname=$(echo "$_srcinfo" | grep '^Source: ' | cut -d' ' -f2)
 _srcver=$(echo "$_srcinfo" | grep '^Version: ' | cut -d' ' -f2)
 
-echo "$_srcver" | grep -q ':' - && {
-        _epoch="$(echo $_srcver | awk -F':' '{print $1}')"
-        _srcver="$(echo $_srcver | awk -F':' '{print $2}')"
-}
+if echo "$_srcver" | grep -q ':'; then
+    _epoch="$(echo $_srcver | awk -F: '{print $1}')"
+    _srcver="$(echo $_srcver | awk -F: '{print $2}')"
+fi
 
 _pkgname=$(grep 'Package: ' debian/control | sed 1q | cut -d' ' -f2)
 _deb=$(find /srv/repository/release/$release/pool -type f -name "${_pkgname}_${_srcver}*.deb" | sort -rg | sed 1q)
 
 if [ -n "$_deb" ] ; then
     _deb=$(basename $_deb)
-    echo "*** deb == $_deb ***"
-
     _buildnum=$(echo $_deb | awk -F'+' '{print $NF}' | cut -d'_' -f1)
+
+    echo "*** deb == $_deb ***"
     echo "*** buildnum == $_buildnum ***"
 
     if echo $_buildnum | grep -q "^${release_num}m7$"; then
@@ -43,8 +43,7 @@ if [ -n "$_deb" ] ; then
     elif echo $_buildnum | grep -q "^${release_num}m7\.."; then
         echo "*** Found previous rebuild. Incrementing build number ***"
         _buildnum=$(echo $_buildnum | awk -F. '{print $NF}')
-        _buildnum=$(expr $_buildnum + 1)
-        _buildnum="${release_num}m7.${_buildnum}"
+        _buildnum="${release_num}m7.$[_buildnum + 1]"
     else
         echo "*** Did not find previous builds. Assuming +${release_num}m7 ***"
         _buildnum="${release_num}m7"
@@ -54,21 +53,18 @@ else
     _buildnum="${release_num}m7"
 fi
 
-_firstline="$(sed 1q debian/changelog)"
-
 [ -n "$_epoch" ] && _srcver="${_epoch}:${_srcver}"
 
 _tempchangelog=$(mktemp)
-tac debian/changelog > $_tempchangelog
-cat <<EOF >> $_tempchangelog
-
- -- Jenkins Auto Builder <spam1@wizzup.org>  $(date +'%a, %d %b %Y %H:%M:%S +0200')
+cat << EOF > $_tempchangelog
+$_srcname (${_srcver}+${_buildnum}) unstable; urgency=medium
 
   * Increment build number
 
-$_srcname (${_srcver}+${_buildnum}) unstable; urgency=medium
+ -- Jenkins Auto Builder <spam1@wizzup.org>  $(date -R)
+
 EOF
+cat debian/changelog >> $_tempchangelog
 
-tac $_tempchangelog > debian/changelog
-rm $_tempchangelog
-
+cat $_tempchangelog > debian/changelog
+rm -f $_tempchangelog
