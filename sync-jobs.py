542697a975a2af420bdf621525d1fd8a0a1f4011
parazyd <parazyd@dyne.org>
2020-03-03 22:59:15 +0100

    Replace default parameters if it's an extras package.
    TODO: Find out how to actually force it.

--- sync-jobs.py
+++ sync-jobs.py
@@ -59,6 +59,16 @@ def create_job_config(japi, jobs, jobname):
         bin_job = bin_job.replace('{{{%s}}}' % r[0], r[1])
         rep_job = rep_job.replace('{{{%s}}}' % r[0], r[1])
 
+    if 'https://github.com/maemo-leste-extras/' in job_info['host']:
+        src_job = src_job.replace('<defaultValue>leste</defaultValue>',
+                                  '<defaultValue>extras</defaultValue>')
+        bin_job = bin_job.replace('<defaultValue>leste</defaultValue>',
+                                  '<defaultValue>extras</defaultValue>')
+        rep_job = rep_job.replace('<defaultValue>leste</defaultValue>',
+                                  '<defaultValue>extras</defaultValue>')
+
+
+
     return (source, src_job), (binaries, bin_job), (repos, rep_job)
 
 
