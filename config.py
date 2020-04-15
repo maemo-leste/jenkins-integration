496ae7ad761b8bf390e9f6aaf78ca0960d5dc685
Jenkins User <spam1@wizzup.org>
2020-04-15 23:14:33 +0100

    config: add hildon-theme-okuda

--- config.py
+++ config.py
@@ -641,6 +641,12 @@ _jobs = OrderedDict([
     }),
 
     # Extra pkg
+    ('hildon-theme-okuda', {
+        'host': 'https://github.com/maemo-leste-extras/%s',
+        'arches': ['all'],
+    }),
+
+    # Extra pkg
     ('maeotp', {
         'host': 'https://github.com/maemo-leste-extras/%s',
     }),
